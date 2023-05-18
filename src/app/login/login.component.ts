f54a2caf70b7cbefb3fab5eff23eede8426366b2
Amine_ben_omrane <benomraneamine3@gmail.com>
2023-05-18 22:31:44 +0200

    first commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 import { NgForm } from "@angular/forms";
 import { BackendApiService } from "app/services/backend-api.service";
-
+import { Router } from '@angular/router';
 @Component({
   selector: "app-login",
   templateUrl: "./login.component.html",
   styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
-  constructor(private auth:BackendApiService) {
+  constructor(private auth:BackendApiService ,  private routes:Router) {
 
   }
   DataResponse :any
@@ -19,11 +19,28 @@ export class LoginComponent implements OnInit {
     this.auth.login(loginForm.value).subscribe((response) =>{  
       // console.log(response);
       this.DataResponse = response
-      console.log(this.DataResponse['access_token'])
-      this.auth.SaveData(this.DataResponse['access_token'])
-
+      //console.log(this.DataResponse);
+      if(this.DataResponse['access_token'] !=null){
+       // console.log(this.DataResponse['access_token'])
+        this.auth.SaveData(this.DataResponse['access_token'])
+        this.routes.navigate(['/dashboard']); 
+      }else{
+        console.log("Wrong")
+      }
+      
+      //this.auth.getData()
     });
 
+  }
+
+  Register(RegisterForm: NgForm) {
+    const formGroup = RegisterForm.form;
+        formGroup.setValue({
+          ...formGroup.value,
+          "role": "user"
+        });
+    console.log(formGroup.value)
+    this.auth.Register(formGroup.value)
   }
   
 }
--- src/app/services/backend-api.service.ts
+++ src/app/services/backend-api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {JwtHelperService} from '@auth0/angular-jwt' ;
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
@@ -9,11 +10,26 @@ export class BackendApiService {
   helper = new JwtHelperService()
   constructor(private http: HttpClient) { }
   login(data:any){
-    return this.http.post('http://localhost:8080/api/v1/auth/authenticate',data)
+    return this.http.post('http://localhost:8020/api/v1/auth/authenticate',data)
+    
+  }
+
+  Register(data:any):Observable<any> {
+    return this.http.post('http://localhost:8080/api/v1/auth/register',data)
     
   }
   SaveData(token: any  ){
     localStorage.setItem('token',token) ; 
-    
+  }
+
+  getData(){
+    let token :any = localStorage.getItem('token') ;
+    if(token!=null){
+      let decode = this.helper.decodeToken(token) ;
+      
+      return decode
+    }else{
+      return "false" 
+    }
   }
 }
